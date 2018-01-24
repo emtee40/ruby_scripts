2032255ca72e11ee48c9d9e603f1ea37dc1df1dc
Paul Sutton <zleap@zleap.net>
2018-01-24 18:29:49 +0000

    add corrected e-mail address

--- string2url/stringurl.rb
+++ string2url/stringurl.rb
@@ -1,6 +1,6 @@
 #  stringurl.rb
 #  
-#  Copyright 2018 Paul Sutton <psutton@desktop>
+#  Copyright 2018 Paul Sutton <zleap@zleap.net>>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
